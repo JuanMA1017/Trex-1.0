5a28f23ed3f1689e7f9d59b177153c31ccc0b00b
JuanMA1017 <95143890+JuanMA1017@users.noreply.github.com>
2021-12-18 11:13:39 -0600

    Add files via upload

---
+++ Clase 15/C11-codigo-con-sangria-main/sketch.js
@@ -0,0 +1,157 @@
+var Trex, imgTrex;
+var piso, imgpiso;
+var pisoinv;
+var imgnube;
+var imgobs1;
+var imgobs2;
+var imgobs3;
+var imgobs4;
+var imgobs5;
+var imgobs6;
+var gamestate = 1;
+var obstaculosg;
+var nubes;
+var imgtrexm;
+var gameover;
+var reset;
+var imggo;
+var imgres;
+var sondie;
+var sonjump;
+var sonchp;
+var score;
+
+function preload(){
+  imgTrex=loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  imgpiso=loadAnimation("ground2.png");
+  imgnube=loadAnimation("cloud.png");
+  imgobs1=loadImage("obstacle1.png");
+  imgobs2=loadImage("obstacle2.png");
+  imgobs3=loadImage("obstacle3.png");
+  imgobs4=loadImage("obstacle4.png");
+  imgobs5=loadImage("obstacle5.png");
+  imgobs6=loadImage("obstacle6.png");
+  imgtrexm=loadAnimation("trex_collided.png");
+  imggo=loadImage("gameOver.png");
+  imgres=loadImage("restart.png");
+  sondie=loadSound("die.mp3");
+  sonjump=loadSound("jump.mp3");
+  sonchp=loadSound("checkpoint.mp3");
+}
+
+function setup(){
+  createCanvas(600, 200);
+  Trex=createSprite(50, 140, 10, 10);
+  Trex.addAnimation("Trexrunning", imgTrex);
+  Trex.addAnimation("Trexmuerto", imgtrexm);
+  Trex.scale=0.7;
+  Trex.setCollider("circle", 0, 0, 50);
+  piso = createSprite (30, 180, 100, 5);
+  piso.addAnimation("Piso", imgpiso);
+  pisoinv=createSprite(30, 190, 100, 5);
+  pisoinv.visible=false;
+  obstaculosg=new Group();
+  nubes=new Group();
+  gameover=createSprite(300, 100, 50, 50);
+  gameover.addImage("Gameover", imggo);
+  gameover.visible=false;
+  reset=createSprite(565, 35, 30, 30);
+  reset.addImage("Restart", imgres);
+  reset.visible=false;
+  score=0;
+}
+
+function draw (){
+  background("lightgray");
+  drawSprites();
+  Trex.collide(pisoinv);
+  textSize(15);
+  text("Score "+score, 20, 12);
+  if(gamestate===1){
+    piso.velocityX=-10;
+    if(score%100===0 && score>0){
+      sonchp.play();
+    }
+    score=score+Math.round(frameCount%1===0);
+    if(keyDown("space")&& Trex.y>=150){
+      sonjump.play();
+      Trex.velocityY=-10;
+    }
+    Trex.velocityY+=0.5;
+    if(obstaculosg.isTouching(Trex)){
+      sondie.play();
+      score=0;
+      gamestate=0;
+    }
+    if(piso.x<0){
+      piso.x=30;
+    }
+    clima();
+    obstaculos();
+  }
+  else if(gamestate===0){
+    Trex.changeAnimation("Trexmuerto", imgtrexm);
+    piso.velocityX=0;
+    nubes.setVelocityXEach(-1);
+    nubes.setLifetimeEach(-1);
+    obstaculosg.setVelocityXEach(0);
+    obstaculosg.setLifetimeEach(-1);
+    gameover.visible=true;
+    gameover.depth+=2;
+    reset.visible=true;
+    reset.depth+=3;
+      if(mousePressedOver(reset)){
+        gameover.visible=false;
+        reset.visible=false;
+        nubes.destroyEach();
+        obstaculosg.destroyEach();
+        Trex.changeAnimation("Trexrunning", imgTrex);
+        gamestate=1;
+      }
+  }
+}
+
+function clima (){
+  if(frameCount%40 === 0){
+    var nube;
+    nube=createSprite (600, 40, 25, 18);
+    nube.addAnimation("Nube", imgnube);
+    nube.scale=0.4;
+    nube.lifetime=65;
+    nube.velocityX=-13;
+    nube.y=Math.round(random(30, 75));
+    gameover,depth=reset.depth;
+    Trex.depth=gameover.depth;
+    nube.depth=Trex.depth;
+    Trex.depth+=1;
+    nubes.add(nube);
+  }
+}
+
+function obstaculos (){
+  if(frameCount%50===0){
+    var obstacle;
+    obstacle=createSprite(600, 160, 5, 5);
+    obstacle.velocityX=-15;
+    var obsr;
+    obsr=Math.round(random(1, 6));
+    switch(obsr){
+      case 1: obstacle.addImage(imgobs1);
+        break;
+      case 2: obstacle.addImage(imgobs2);
+        break;
+      case 3: obstacle.addImage(imgobs3);
+        break;
+      case 4: obstacle.addImage(imgobs4);
+        break;
+      case 5: obstacle.addImage(imgobs5);
+        break;
+      case 6: obstacle.addImage(imgobs6);
+        break;
+      default: break;
+    }
+    obstacle.scale=0.7;
+    obstacle.lifetime=60;
+    obstaculosg.add(obstacle);
+  }
+}
